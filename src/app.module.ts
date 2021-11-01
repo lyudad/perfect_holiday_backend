a3f941723126055a9f3d8d0ad7fe02378b134840
Holovashchenko <holovashcenko@gmail.com>
2021-11-02 00:08:42 +0200

    Added configuration properties to app.module.ts for mysql

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,18 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'test',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
