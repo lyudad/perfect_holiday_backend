b3e6a8f87e706b0384f04705731987c602affab9
Holovashchenko <holovashcenko@gmail.com>
2021-11-03 22:31:07 +0200

    Fix bug 2, was deleted next dirs: employee, admin, super

--- src/admin/admin.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AdminService } from './services/admin.service';
-import { AdminController } from './controllers/admin.controller';
-
-@Module({
-  providers: [AdminService],
-  controllers: [AdminController]
-})
-export class AdminModule {}
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,18 +2,8 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
-import { EmployeeModule } from './employee/employee.module';
-import { AdminModule } from './admin/admin.module';
-import { SuperModule } from './super/super.module';
 @Module({
-  imports: [
-    TypeOrmModule.forRoot(),
-    UsersModule,
-    AuthModule,
-    EmployeeModule,
-    AdminModule,
-    SuperModule,
-  ],
+  imports: [TypeOrmModule.forRoot(), UsersModule, AuthModule],
   controllers: [],
   providers: [],
 })
