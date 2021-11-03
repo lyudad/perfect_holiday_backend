3426fa95f43af112469b3f50139dc2d94c2b3b2f
Holovashchenko <holovashcenko@gmail.com>
2021-11-03 22:44:44 +0200

    Created profile module, controller, service, entity.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { ProfileModule } from './profile/profile.module';
 @Module({
-  imports: [TypeOrmModule.forRoot(), UsersModule, AuthModule],
+  imports: [TypeOrmModule.forRoot(), UsersModule, AuthModule, ProfileModule],
   controllers: [],
   providers: [],
 })
