684a90a0b9cb9a9b839f4fbcc7c2bc6abe08325e
Yaroslav <yaroslav.brila@gmail.com>
2021-11-11 01:02:22 +0200

    added delete endpoint and cors

--- src/users/controllers/users.controller.ts
+++ src/users/controllers/users.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Param } from '@nestjs/common';
+import { UsersService } from '../services/users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {
+  }
+  @Delete(':id')
+  deleteAction(@Param('id') id: string) : Promise<void>{
+    return this.usersService.remove(id);
+  }
+}
--- src/users/services/users.service.ts
+++ src/users/services/users.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Users } from '../../entity/Users.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(Users)
+    private readonly usersRepository: Repository<Users>)
+  {}
+  async remove(id:string):Promise<void>{
+    await this.usersRepository.delete(id);
+  }
+}
