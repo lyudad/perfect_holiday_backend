32224dafb067f83ee6b3edb2715204c92b23492f
Alex <tverdovskyi92@gmail.com>
2021-11-11 23:31:47 +0200

    Changes

--- src/auth/services/auth.service.ts
+++ src/auth/services/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { Users } from 'src/users/entities/user.entity';
 import { UsersService } from 'src/users/services/users.service';
 import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class AuthService {
@@ -11,12 +12,18 @@ export class AuthService {
   ) {}
 
   async validate(email: string, password: string): Promise<Users> | null {
-    const user = await this.userService.getUserByEmail(email);
-    if (!user) {
-      return null;
+    try {
+      const user = await this.userService.getUserByEmail(email);
+      if (!user) {
+        return null;
+      }
+      const passEq = bcrypt.compare(password, user.password);
+      if (user && passEq) {
+        return user;
+      }
+    } catch (error) {
+      error.message;
     }
-    const passwordIsValid = password === user.password;
-    return passwordIsValid ? user : null;
   }
 
   async login(user: Users): Promise<{ access_token: string }> {
