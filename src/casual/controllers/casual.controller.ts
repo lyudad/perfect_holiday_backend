afa0b3d840d50335f441ddf578333f2f7ecd1e96
Holovashchenko Vladyslav <holovashcenko@gmail.com>
2021-11-16 19:54:58 +0200

    Realized Methods: GET all list with rest days, add rest days for users, upprove or delete rest request

--- src/casual/controllers/casual.controller.ts
+++ src/casual/controllers/casual.controller.ts
@@ -1,4 +1,64 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Header,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+  Put,
+  UseGuards,
+} from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/strategies/guards/jwt-auth.guard';
+import { Vacations } from 'src/entity/Vacations.entity';
+import { CasualService } from '../services/casual.service';
 
 @Controller('casual')
-export class CasualController {}
+export class CasualController {
+  constructor(private readonly casualService: CasualService) {}
+
+  //  GET  /casual   Находит все отпуска работкиков
+  // @UseGuards(JwtAuthGuard)
+  @Get()
+  findAll(): Promise<Vacations[]> {
+    return this.casualService.findAll();
+  }
+
+  // GET   /casual/id   Находит все выходные пользователя по его id
+  // @UseGuards(JwtAuthGuard)
+  @Get(':id')
+  findAllRestDays(@Param('id') id: string): Promise<Vacations> {
+    return this.casualService.findAllRestDays(id);
+  }
+
+  // POST /casual/id Создает начальный день, конечный день и тип отпуска
+  @Post(':id')
+  @HttpCode(HttpStatus.CREATED)
+  @Header('Cache-Control', 'none')
+  create(
+    @Body() createRestday,
+    @Param('id') idfrompath: string,
+  ): Promise<Vacations> {
+    return this.casualService.create(createRestday, idfrompath);
+  }
+
+  // PUT /casual/id Подтверждаем или отклоняем отпуск (Меняет поле STATUS   approve | pending)
+  @Put(':id')
+  updateStatus(
+    @Body() changeStatus,
+    @Param('id') idfrompath: string,
+  ): Promise<Vacations> {
+    return this.casualService.updateStatus(changeStatus, idfrompath);
+  }
+
+  // DELETE  /casual/id   Удаляет забронированый отпуск с таблицы по id vacations
+  @Delete(':id')
+  deleteRestDay(
+    @Body() deleteRest,
+    @Param('id') idfrompath: string,
+  ): Promise<Vacations> {
+    return this.casualService.deleteRestDay(deleteRest, idfrompath);
+  }
+}
--- src/casual/services/casual.service.ts
+++ src/casual/services/casual.service.ts
@@ -1,4 +1,54 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Vacations } from 'src/entity/Vacations.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class CasualService {}
+export class CasualService {
+  constructor(
+    @InjectRepository(Vacations)
+    private readonly casualRepository: Repository<Vacations>,
+  ) {}
+
+  // Находит все отпуска работкиков
+  async findAll(): Promise<Vacations[]> {
+    return this.casualRepository.query(`
+    SELECT ${process.env.DB}.vacations.*, ${process.env.DB}.users.*
+    FROM ${process.env.DB}.vacations 
+    INNER JOIN ${process.env.DB}.users 
+    ON (${process.env.DB}.vacations.userId=${process.env.DB}.users.id)
+    `);
+  }
+
+  // Находит все выходные пользователя по его id
+  async findAllRestDays(id): Promise<Vacations> {
+    return this.casualRepository.query(`
+      SELECT ${process.env.DB}.vacations.*, ${process.env.DB}.users.last_name
+      FROM ${process.env.DB}.vacations 
+      INNER JOIN ${process.env.DB}.users 
+      ON (${process.env.DB}.vacations.userId=${process.env.DB}.users.id)
+      WHERE ${process.env.DB}.vacations.userId=${id}
+    `);
+  }
+
+  // Создает начальный день, конечный день и тип отпуска
+  async create(createRestday, idfrompath): Promise<Vacations> {
+    return this.casualRepository.query(`
+    INSERT INTO ${process.env.DB}.vacations (start_date, end_date, type, userId) VALUES ('${createRestday.start_date}', '${createRestday.end_date}', '${createRestday.type}', ${idfrompath}) 
+    `);
+  }
+
+  // Подтверждает или отклоняет отпуск (Меняет поле STATUS   approve | pending)
+  async updateStatus(changeStatus, idfrompath): Promise<Vacations> {
+    return this.casualRepository.query(`
+      UPDATE ${process.env.DB}.vacations SET status = '${changeStatus.status}' WHERE (id = '${changeStatus.id}' AND userId = '${idfrompath}');
+    `);
+  }
+
+  // Удаляет забронированый отпуск с таблицы по id vacations
+  async deleteRestDay(deleteRest, idfrompath): Promise<Vacations> {
+    return this.casualRepository.query(`
+      DELETE FROM ${process.env.DB}.vacations WHERE (id = '${deleteRest.id}' AND userId = '${idfrompath}');
+    `);
+  }
+}
