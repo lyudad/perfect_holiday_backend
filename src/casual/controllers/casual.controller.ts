1529a4f434413c1de1526ea38ded257ce4f64f0c
Holovashchenko Vladyslav <holovashcenko@gmail.com>
2021-11-24 11:01:18 +0200

    Refactoring i casual dir

--- src/casual/controllers/casual.controller.ts
+++ src/casual/controllers/casual.controller.ts
@@ -18,42 +18,42 @@ import { CasualService } from '../services/casual.service';
 export class CasualController {
   constructor(private readonly casualService: CasualService) {}
 
-  //  GET  /casual   Находит все отпуска работкиков
   @UseGuards(JwtAuthGuard)
   @Get()
   findAll() {
     return this.casualService.findAll();
   }
 
-  //  GET  /casual/pending   Находит все не подтвержденныеч выходные пользователей(со статусом PENDING)
   @UseGuards(JwtAuthGuard)
   @Get('pending')
   findAllNotApprovedRestDays() {
     return this.casualService.findAllNotApprovedRestDays();
   }
 
-  // GET   /casual/id   Находит все выходные пользователя по его id
   @UseGuards(JwtAuthGuard)
   @Get(':id')
   findAllRestDays(@Param('id') id: string) {
     return this.casualService.findAllRestDays(id);
   }
 
-  // POST /casual/id Создает начальный день, конечный день и тип отпуска
   @Post(':id')
   @HttpCode(HttpStatus.CREATED)
   @Header('Cache-Control', 'none')
-  create(@Body() createRestday, @Param('id') idfrompath: string) {
-    return this.casualService.create(createRestday, idfrompath);
+  createStartAndLastRestDay(
+    @Body() createRestday,
+    @Param('id') idfrompath: string,
+  ) {
+    return this.casualService.createStartAndLastRestDay(
+      createRestday,
+      idfrompath,
+    );
   }
 
-  // PUT /casual/id Подтверждаем или отклоняем отпуск (Меняет поле STATUS   approve | pending)
   @Put(':id')
   updateStatus(@Body() changeStatus, @Param('id') idfrompath: string) {
     return this.casualService.updateStatus(changeStatus, idfrompath);
   }
 
-  // DELETE  /casual/id   Удаляет забронированый отпуск с таблицы по id vacations
   @Delete(':id')
   deleteRestDay(@Body() deleteRest, @Param('id') idfrompath: string) {
     return this.casualService.deleteRestDay(deleteRest, idfrompath);
