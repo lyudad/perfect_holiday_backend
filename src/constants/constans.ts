da6a926a4bf4c806d54239c715840045ad7e95ef
Holovashchenko Vladyslav <holovashcenko@gmail.com>
2021-12-02 11:03:35 +0200

    Realised functionality: autoupdate vacation and sick days on the New Year Day

--- src/constants/constans.ts
+++ src/constants/constans.ts
@@ -1,5 +1,22 @@
+import { CronOptions } from '@nestjs/schedule';
+
 export enum Roles {
   EMPLOYEE = 'employee',
   ADMIN = 'admin',
   SUPER = 'super',
 }
+type TNewYearUpdatesDays = {
+  QUANTITY_AVAILABLE_VACATION: number;
+  QUANTITY_AVAILABLE_SICK_DAYS: number;
+  CRON_DATE: string | Date;
+  CRON_OPTIONS: CronOptions;
+};
+export const NewYearUpdatesDays: TNewYearUpdatesDays = {
+  QUANTITY_AVAILABLE_VACATION: 15,
+  QUANTITY_AVAILABLE_SICK_DAYS: 5,
+  CRON_DATE: '0 0 0 1 1 *',
+  CRON_OPTIONS: {
+    name: 'notifications',
+    timeZone: 'Europe/Kiev',
+  },
+};
