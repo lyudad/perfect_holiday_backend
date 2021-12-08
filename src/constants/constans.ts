21739b9fa4423a7fdf7041b036d3ec2c8ae720e6
Yaroslav <yaroslav.brila@gmail.com>
2021-12-08 22:51:57 +0200

    delete new year update

--- src/constants/constans.ts
+++ src/constants/constans.ts
@@ -8,14 +8,12 @@ export enum Roles {
 type TNewYearUpdatesDays = {
   QUANTITY_AVAILABLE_VACATION: number;
   QUANTITY_AVAILABLE_SICK_DAYS: number;
-  QUANTITY_AVAILABLE_UNPAID_DAYS: number;
   CRON_DATE: string | Date;
   CRON_OPTIONS: CronOptions;
 };
 export const NewYearUpdatesDays: TNewYearUpdatesDays = {
   QUANTITY_AVAILABLE_VACATION: 15,
   QUANTITY_AVAILABLE_SICK_DAYS: 5,
-  QUANTITY_AVAILABLE_UNPAID_DAYS: 1000,
   CRON_DATE: '0 0 0 1 1 *',
   CRON_OPTIONS: {
     name: 'notifications',
